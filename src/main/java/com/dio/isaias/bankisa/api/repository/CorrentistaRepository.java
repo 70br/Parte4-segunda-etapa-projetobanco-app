92036f103a65a4128f893c6477918f78a02a6083
70br <elias70br@hotmail.com>
2022-06-30 09:27:52 -0300

    subir o final

---
+++ src/main/java/com/dio/isaias/bankisa/api/repository/CorrentistaRepository.java
@@ -0,0 +1,10 @@
+package com.dio.isaias.bankisa.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dio.isaias.bankisa.api.model.Correntista;
+
+public interface CorrentistaRepository extends JpaRepository<Correntista, Integer>{
+	
+
+}
---
+++ src/main/java/com/dio/isaias/bankisa/api/repository/MovimentacaoRepository.java
@@ -0,0 +1,10 @@
+package com.dio.isaias.bankisa.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dio.isaias.bankisa.api.model.Movimentacao;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao,Integer >{
+	
+
+}

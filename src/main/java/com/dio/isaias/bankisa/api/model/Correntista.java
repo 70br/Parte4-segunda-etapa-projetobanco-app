e92c02bc4f92366866bf93654f8c5a12a02edbc1
70br <elias70br@hotmail.com>
2024-03-26 11:15:45 -0300

    subir

--- src/main/java/com/dio/isaias/bankisa/api/model/Correntista.java
+++ src/main/java/com/dio/isaias/bankisa/api/model/Correntista.java
@@ -1,13 +1,12 @@
 package com.dio.isaias.bankisa.api.model;
 
-import javax.annotation.Generated;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @Entity
